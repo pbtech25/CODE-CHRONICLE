f03dff53e943afac1f1577bffe6d1c5a91ffc9d5
pbtech25 <pbtech25@gmail.com>
2023-11-06 20:04:38 +0530

    Did some major changes

--- arrayInput.java
+++ arrayInput.java
@@ -1,7 +1,7 @@
 package ARRAYS;
 import java.util.*;
 
-public class array_input {
+public class arrayInput {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -16,6 +16,23 @@ public class array_input {
         System.out.println("Physics: " + marks[0]);
         System.out.println("Chemistry: " + marks[1]);
         System.out.println("Maths: " + marks[2]);
+
+        marks[2] = 100;
+        System.out.println("Updated Maths Marks: " + marks[2]);
+
+        marks[1] = marks[1] +10;
+        System.out.println("Increased Chemistry Marks by 10: " + marks[1]);
+
+        //finding percenatges
+        float percentage = 0f;
+
+        for (int i = 0; i <=2; i++) {
+            percentage += marks[i];
+        }
+        float per = percentage/3;
+
+
+        System.out.println("Percentage: " + per/3);
 
         sc.close();
     }
