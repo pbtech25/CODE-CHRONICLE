c18330ec98bcaeae0a94317aebc35f75b3e16832
pbtech25 <pbtech25@gmail.com>
2023-11-06 23:39:30 +0530

    Modified Largest Number code

--- largestNumber.java
+++ largestNumber.java
@@ -3,23 +3,27 @@ import java.util.*;
 public class largestNumber {
     public static int largest_Number(int num[]){
         int smallest = Integer.MIN_VALUE;
-        int largest = 0;
+        int largest = Integer.MAX_VALUE;
 
         for (int i = 0; i < num.length; i++) {
             if (num[i] > smallest) {
+                smallest = num[i];
+                //smallest = largest;
+            }
+
+            if (num[i] < largest) {
                 largest = num[i];
-                smallest = largest;
             }
         }
-        
-        return largest;
+        System.out.println("The smallest number is: " + largest);
+        return smallest;
         
     }
     public static void main(String[] args) {
         int num[] = {1,2,26,3,45, 0, -12, 9};
 
-        int largest = largest_Number(num);
+        //int largest = largest_Number(num);
 
-        System.out.println("The largest number is: " + largest);
+        System.out.println("The largest number is: " + largest_Number(num));
     }
 }
