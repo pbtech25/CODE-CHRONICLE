8f0f2839870b9986fe96bb6cd452cb3e718c06c9
pbtech25 <pbtech25@gmail.com>
2023-11-06 20:39:29 +0530

    Modified arrayAsFunction

--- arraysAsFunction.java
+++ arraysAsFunction.java
@@ -2,18 +2,22 @@ import java.util.*;
 
 public class arraysAsFunction {
 
-    public static void update(int marks[]){
+    public static void update(int marks[], int nonChangeable){
+        nonChangeable = 12344;
         for (int i = 0; i < marks.length; i++) {
             marks[i] = marks[i] + 1;
         }
     }
     public static void main(String[] args) {
         int marks[] = {97, 98, 99}; 
-        update(marks);
+        int nonChangeable = 5;
+        update(marks, nonChangeable);
 
         for (int i = 0; i < marks.length; i++) {
             System.out.print(marks[i] + " ");
             
         }
+        System.out.println();
+        System.out.println(nonChangeable);
     }
 }
