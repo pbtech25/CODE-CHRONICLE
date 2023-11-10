a3db6064f0557eac43a8bf56ae794aa71887a75e
pbtech25 <pbtech25@gmail.com>
2023-11-10 23:20:26 +0530

    code for finding printSubarrays

---
+++ printSubarrays.java
@@ -0,0 +1,23 @@
+public class printSubarrays {
+    public static void print_subarrays(int arr[]) {
+        int ts = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                for (int k = i; k <= j; k++) {
+                    System.out.print(arr[k] + " ");
+                }
+                ts++;
+                System.out.println();
+            }
+            System.out.println();
+        }
+        System.out.println("Total number of subarrays: " + ts);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 2, 4, 6, 8, 10 };
+        System.out.println("The sub arrays are:");
+
+        print_subarrays(arr);
+    }
+}
