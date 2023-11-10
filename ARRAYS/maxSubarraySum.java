03adbda7d243baabc95e38608b6a6af364b0a812
pbtech25 <pbtech25@gmail.com>
2023-11-11 00:54:50 +0530

    Modified Subarray maximum sum code

--- ARRAYS/maxSubarraySum.java
+++ ARRAYS/maxSubarraySum.java
@@ -1,9 +1,32 @@
-public class maxSubarraySum {
-    public static void max_subarray_sum(int arr[]) {
+//import java.util.*;
 
+public class maxSubarraySum {
+    public static int max_subarray_sum(int arr[]) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                currSum = 0;
+                for (int k = i; k <= j; k++) {
+                    currSum += arr[k];
+                }
+                System.out.println(currSum);
+
+                if (maxSum < currSum) {
+                    maxSum = currSum;
+                }
+            }
+        }
+        return maxSum;
     }
 
     public static void main(String[] args) {
         int arr[] = { 1, -2, 6, 1, -3 };
+
+        int max_sum = max_subarray_sum(arr);
+
+        System.out.println("Maximum subarray sum is: " + max_sum);
+
     }
 }
