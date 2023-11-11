6b2048777430342e196d47c7adb2dbe7e3034dbd
pbtech25 <pbtech25@gmail.com>
2023-11-11 09:00:11 +0530

    Code for Kadane's Algorithm

--- ARRAYS/kadanesAlgo.java
+++ ARRAYS/kadanesAlgo.java
@@ -1,5 +1,26 @@
 public class kadanesAlgo {
-    public static void main(String[] args) {
+    public static int kadanes(int arr[]) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
 
+        for (int i = 0; i < arr.length; i++) {
+            currSum += arr[i];
+
+            if (currSum < 0) {
+                currSum = 0;
+            }
+
+            maxSum = Math.max(maxSum, currSum);
+        }
+        return maxSum;
+
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
+
+        int max_sum = kadanes(arr);
+
+        System.out.println("Maximum SubArray Sum is: " + max_sum);
     }
 }
