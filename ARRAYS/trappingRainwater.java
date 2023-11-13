5bcbb5467eb74b74e874f25e25faa959faa20869
pbtech25 <pbtech25@gmail.com>
2023-11-13 23:56:42 +0530

    Modified Trapping Rainwater code

--- ARRAYS/trappingRainwater.java
+++ ARRAYS/trappingRainwater.java
@@ -3,6 +3,9 @@ public class trappingRainwater {
         // calculate left max boundary
         int leftMax[] = new int[height.length];
         leftMax[0] = height[0];
+        for (int i = 1; i < height.length; i++) {
+            leftMax[i] = Math.max(height[i], leftMax[i - 1]);
+        }
 
         // calculate reght max boundary
 
