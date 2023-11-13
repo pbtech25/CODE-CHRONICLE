73629e76b5b006d8e926d10215fd9084cbd26c53
pbtech25 <pbtech25@gmail.com>
2023-11-14 02:04:33 +0530

    Code for Buying and Selling Stocks

---
+++ ARRAYS/buyNsellStocks.java
@@ -0,0 +1,24 @@
+public class buyNsellStocks {
+    public static int buyNsell(int prices[]) {
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (buyPrice < prices[i]) {
+                int profit = prices[i] - buyPrice;
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                buyPrice = prices[i];
+            }
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String args[]) {
+        int prices[] = { 7, 1, 5, 3, 6, 4 };
+
+        int max_profit = buyNsell(prices);
+        System.out.println("maximum profit is: " + max_profit);
+    }
+}
