b4e283baae2d7a58f227619de29b604f616aaa4a
pbtech25 <pbtech25@gmail.com>
2023-11-23 00:23:04 +0530

    Modified Code Code for Bubble Sort

--- BASIC SORTING ALGORITHMS/bubbleSort.java
+++ BASIC SORTING ALGORITHMS/bubbleSort.java
@@ -1,9 +1,9 @@
-public class bubbleSort{
-    public static void bubble_sort(int arr[]){
-        for(int turn = 0; turn < arr.length -1; turn++){
-            for(int j=0; j<arr.length-turn-1; j++){
-                if (arr[j] > arr[j+1]) {
-                    //swap
+public class bubbleSort {
+    public static void bubble_Sort(int arr[]) {
+        for (int turn = 0; turn < arr.length - 1; turn++) {
+            for (int j = 0; j < arr.length - 1 - turn; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
@@ -11,14 +11,19 @@ public class bubbleSort{
             }
         }
 
-        for(int k : arr){
+        for (int k : arr) {
             System.out.print(k + " ");
         }
     }
 
-    public static void main(String args[]){
-        int arr[] = {5,4,1,3,2};
+    public static void main(String[] args) {
+        int arr[] = { 5, 4, 3, 1, 2 };
 
-        bubble_sort(arr);
+        bubble_Sort(arr);
     }
 }
+
+
+
+
+
