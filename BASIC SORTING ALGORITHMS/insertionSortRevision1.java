5a57e60cca91512c3e621733d6ab0261458577b7
pbtech25 <pbtech25@gmail.com>
2023-11-27 23:29:35 +0530

    Implemented Arrays.sort()

--- BASIC SORTING ALGORITHMS/insertionSortRevision1.java
+++ BASIC SORTING ALGORITHMS/insertionSortRevision1.java
@@ -1,3 +1,5 @@
+import java.util.Arrays; //also can import java.util.*;
+
 public class insertionSortRevision1 {
     public static void insertion_sort(int arr[]) {
         for (int i = 1; i < arr.length; i++) {
@@ -21,6 +23,12 @@ public class insertionSortRevision1 {
     public static void main(String[] args) {
         int arr[] = { 5, 1, 4, 3, 2 };
 
-        insertion_sort(arr);
+        // insertion_sort(arr);
+
+        Arrays.sort(arr);
+        System.out.println("Arry sorted by Arrays.sort() : ");
+        for (int k : arr) {
+            System.out.print(k + " ");
+        }
     }
 }
