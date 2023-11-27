7a5cce41762a045953cb66e1e58ea549eadec185
pbtech25 <pbtech25@gmail.com>
2023-11-27 23:19:16 +0530

    Revised Insertion Sort

---
+++ BASIC SORTING ALGORITHMS/insertionSortRevision1.java
@@ -0,0 +1,26 @@
+public class insertionSortRevision1 {
+    public static void insertion_sort(int arr[]) {
+        for (int i = 1; i < arr.length; i++) {
+            int curr = arr[i];
+            int prev = i - 1;
+
+            while (prev >= 0 && arr[prev] > curr) {
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+
+            arr[prev + 1] = curr;
+        }
+
+        // print
+        for (int k : arr) {
+            System.out.print(k + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 1, 4, 3, 2 };
+
+        insertion_sort(arr);
+    }
+}
