bfb1e781d8ef689c221500cf7120afdd342eb43c
pbtech25 <pbtech25@gmail.com>
2023-11-30 23:25:48 +0530

    Solved Assignment of SelectionSort

--- BASIC SORTING ALGORITHMS/ASSIGNMENT/sorting.java
+++ BASIC SORTING ALGORITHMS/ASSIGNMENT/sorting.java
@@ -21,6 +21,24 @@ public class sorting {
     }
 
     public static void selection_sort(int arr[]) {
+        // outer loop 0 -> n-2
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+
+            // inner loop for minIndex in unsorted array
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // swap
+            if (minIndex != i) {
+                int temp = arr[minIndex];
+                arr[minIndex] = arr[i];
+                arr[i] = temp;
+            }
+        }
 
         // print
         for (int k : arr) {
@@ -47,11 +65,11 @@ public class sorting {
     public static void main(String[] args) {
         int arr[] = { 3, 6, 2, 1, 8, 7, 4, 5, 3, 1 };
 
-        System.out.println("Bubble sorting: ");
-        bubble_sort(arr);
+        // System.out.println("Bubble sorting: ");
+        // bubble_sort(arr);
 
-        // System.out.println("selection sorting: ");
-        // selection_sort(arr);
+        System.out.println("selection sorting: ");
+        selection_sort(arr);
 
         // System.out.println("insertion sorting: ");
         // insertion_sort(arr);
