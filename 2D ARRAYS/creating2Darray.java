67d14e5520145038508cf871f6c57b8e58db308b
pbtech25 <pbtech25@gmail.com>
2023-12-03 00:28:20 +0530

    Added func. to find largest and smallest element in 2D-Array

--- 2D ARRAYS/creating2Darray.java
+++ 2D ARRAYS/creating2Darray.java
@@ -1,6 +1,34 @@
 import java.util.*;
 
 public class creating2Darray {
+    public static int largest_element(int matrix[][]) {
+        int largest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] > largest) {
+                    largest = matrix[i][j];
+                }
+            }
+        }
+
+        return largest;
+    }
+
+    public static int smallest_element(int matrix[][]) {
+        int smallest = Integer.MAX_VALUE;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] < smallest) {
+                    smallest = matrix[i][j];
+                }
+            }
+        }
+
+        return smallest;
+    }
+
     public static boolean search(int matrix[][], int key) {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
@@ -33,7 +61,14 @@ public class creating2Darray {
             System.out.println();
         }
 
+        // SEARCHING ELEMENT
         search(matrix, 5);
+
+        // SMALLEST ELEMENT
+        System.out.println("Smallest element is: " + smallest_element(matrix));
+
+        // LARGEST ELEMENT
+        System.out.println("Largest element is: " + largest_element(matrix));
 
         sc.close();
     }
