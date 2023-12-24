11f0465ac6ce19d4881599b4cf1622504e50d90c
pbtech25 <pbtech25@gmail.com>
2023-12-25 00:12:48 +0530

    Modified bubbleSortRevision2.java

--- BASIC SORTING ALGORITHMS/bubbleSortRevision2.java
+++ BASIC SORTING ALGORITHMS/bubbleSortRevision2.java
@@ -1,6 +1,6 @@
 public class bubbleSortRevision2 {
     public static void bubbleSort(int array[]) {
-        for (int i = 0; i < array.length - 1; i++) {
+        for (int i = 1; i <= array.length - 1; i++) {
             for (int j = 0; j < array.length - i - 1; j++) {
                 if (array[j] > array[j + 1]) {
                     // swap
