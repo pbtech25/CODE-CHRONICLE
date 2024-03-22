1932e6168de3197f2c783c43be6f281443854f85
pbtech25 <pbtech25@gmail.com>
2024-03-22 23:55:15 +0530

    Leetcode solution for 1.Two Sum

---
+++ LEETCODE/Two_Sum.java
@@ -0,0 +1,14 @@
+package LEETCODE;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class Two_Sum {
+    public static void main(String[] args) {
+        Map<Integer, String> phonebook = new HashMap<>();
+
+        phonebook.put(123, "Alice");
+        phonebook.put(456, "Bob");
+        System.out.println(phonebook.get(123));
+    }
+}
